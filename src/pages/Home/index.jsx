2fb85385e6a861057ab1ffca13bf52956a7403ce
isabella <isabellaoliveira8283@gmail.com>
2024-06-21 19:26:45 -0300

    atualização pagina home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,13 +3,13 @@ import imagem from './imagem.png'
 
 export default function Home() {
     return (
-        <div className='flex flex-row'>
-            <div className="bg-gray-200 items-center justify-center w-3/6">
-                <h1>Administre suas compras em um só lugar</h1>
+        <div className='container-home'>
+            <div className="container-direito">
+                <h1 className='titulo-home'>Administre suas compras em um só lugar</h1>
 
-                <p>Gerencie cotações e fornecedores de forma simples e eficiente. Cadastre, acompanhe e gerencie todas as cotações e fornecedores em um só lugar.</p>
+                <p className='texto'>Gerencie cotações e fornecedores de forma simples e eficiente. Cadastre, acompanhe e gerencie todas as cotações e fornecedores em um só lugar.</p>
             </div>
-            <div className="w-3/6">
+            <div className="container-esquerdo">
                 <img className='w-3/5' src={imagem} alt="" />
             </div>
         </div>
